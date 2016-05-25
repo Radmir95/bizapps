55f7f4645d8a2489c31241f142c1f460e6d0ea60
Husnutdinov Radmir <XTreme95@yandex.ru>
2016-05-26 00:27:42 +0300

    AddFiles

---
+++ Employee.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace ITHRDepartment{
+
+    public class Employee {
+        private String FIO;
+        private String role;
+        private String position;
+        private String telephone;
+        private String email;
+        private String lead;
+        private String subdivision;
+
+        // data structure where we are keeping our subdivisions
+        private static ArrayList subdvisions = new ArrayList();
+        // data structure where we are keeping positions of emplyees
+        private static ArrayList positions = new ArrayList();
+
+        public Employee(String FIO, String role, String position, String telephone, String email, String lead, String subdivision) {
+            this.FIO = FIO;
+            this.role = role;
+            this.position = position;
+            this.telephone = telephone;
+            this.email = email;
+            this.lead = lead;
+            this.subdivision = subdivision;
+            positions.Add("junior");
+            positions.Add("middle");
+            positions.Add("senior");
+            positions.Add("team lead");
+            subdvisions.Add("09305");
+            subdvisions.Add("09306");
+            subdvisions.Add("09307");
+            subdvisions.Add("09308");
+
+        }
+
+       
+        public void downPosition()
+        {
+            int rank = positions.IndexOf(this.position);
+            if (rank != 0) {
+                this.position = (String)positions[rank-1];
+            }
+
+
+        }
+
+        public void upPosition()
+        {
+            int rank = positions.IndexOf(this.position);
+            if (rank != 3)
+            {
+                this.position = (String)positions[rank + 1];
+            }
+
+        }
+
+
+        public void changeTelephone(String newTelephone) {
+            this.telephone = newTelephone;
+        }
+
+        public void changeEmail(String newEmail) {
+            this.email = newEmail;
+        }
+
+        // change subdivision for employee
+        public void changeSubdivision(String newSubdivision)
+        {
+            if (subdvisions.Contains(newSubdivision))
+                this.subdivision = newSubdivision;
+        }
+
+        // Working with subdivisions
+
+            //add new subdivision
+        public void addSubdivision(String newSubdivision) {
+            if (!subdvisions.Contains(newSubdivision))
+                subdvisions.Add(newSubdivision);
+        }
+            //delete subdivision
+        public void deleteSubdivision(String subdivision)
+        {
+            if (subdvisions.Contains(subdivision))
+                subdvisions.Remove(subdivision);
+        }
+
+            // edit subdivision. If subdivision will change name
+        public void editSubdivision(String oldSubdivision, String newSubdivision) {
+            if (subdvisions.Contains(oldSubdivision)) {
+                subdvisions.Remove(oldSubdivision);
+                subdvisions.Add(newSubdivision);
+            }
+        }
+
+        public void printInformation() {
+            Console.WriteLine(FIO + " " + role + " " + position + " " + telephone + " " + email + " " + lead + " " +subdivision);
+
+        }
+
+        // SECTION FOR WORKING WITH FILES
+
+        public static ArrayList readFromFile(){
+
+            FileStream file = new FileStream("C:/Users/Radmir.XTREME95/Desktop/employee.txt", FileMode.Open, FileAccess.Read);
+            ArrayList list = new ArrayList();
+            StreamReader reader = new StreamReader(file);
+            while (!reader.EndOfStream)
+            {
+                string s = reader.ReadLine();
+                string[] newStr = s.Split(' ');
+                string[] data = new string[7];
+                string k = newStr[0] + " " + newStr[1] + " " +  newStr[2];
+                data[0] = k;
+                data[1] = newStr[3];
+                data[2] = newStr[4];
+                data[3] = newStr[5];
+                data[4] = newStr[6]+ " " +newStr[8];
+                data[6] = newStr[9];
+
+                list.Add(data);
+
+            }
+            reader.Close();
+            return list;
+            
+        }
+
+        public static void WriteToFile(Employee empl) {
+            FileStream file1 = new FileStream("C:/Users/Radmir.XTREME95/Desktop/employeeWrite.txt", FileMode.OpenOrCreate);
+            StreamWriter writer = new StreamWriter(file1);
+            writer.WriteLine(empl.FIO + " " + " " + empl.role + " " + empl.position + " " + empl.telephone + " " + empl.email + " " + empl.lead + " " + empl.subdivision);
+            writer.Close();
+
+        }
+
+
+    }
+
+   
+
+   
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITHRDepartment;
+
+namespace Start{
+
+    class Program{
+
+        static void Main(string[] args){
+            Employee empl1 = new Employee("Dmitriy Ivanovich Naumov", "developer", "middle", "8-960-152-65-78", "IvanovichDmitriy92@yandex.ru", "Ivan Sergeevich", "09305");
+            Employee empl2 = new Employee("Dmitriy Urievich Hasanov", "qa", "team lead", "8-960-152-65-78", "DmitryHasanov@gmail.com", "Ivan Sergeevich", "09305");
+            Employee empl3 = new Employee("Andrey Ivanovich Nabiullin", "project manager", "junior", "8-960-152-65-78", "andrey123@yandex.ru", "Dmitriy Kasapov", "09307");
+            Employee empl4 = new Employee("Alexey Ivanovich Kuznezov", "bussiness analyst", "middle", "8-960-152-65-78", "alexis@yandex.ru", "Dmitriy Kasapov", "09307");
+            Employee empl5 = new Employee("Petr Ivanovich Vasnecov", "product owner", "senior", "8-960-152-65-78", "petrvasnec@gmail.com", "Alexey Naumovich", "09308");
+
+            Console.WriteLine("down in position");
+            empl1.printInformation();
+            empl1.downPosition();
+            empl1.printInformation();
+
+            Console.WriteLine("change email");
+            empl1.changeEmail("svngdsn@yandex.ru");
+            empl1.printInformation();
+
+            Console.WriteLine("change subdivision");
+            empl1.changeSubdivision("09308");
+            empl1.printInformation();
+
+            Console.WriteLine("Attemp to change subdivison on 09309, but nothing to happen, because this position doesn't exist");
+            empl1.changeSubdivision("09309");
+            empl1.printInformation();
+
+            Console.WriteLine("add new subdivision");
+            empl1.addSubdivision("09309");
+            empl1.changeSubdivision("09309");
+            empl1.printInformation();
+
+            Console.WriteLine("loading from file 'employee.txt'");
+            ArrayList list = Employee.readFromFile();
+            String[] t = (String[])list[0];
+            Console.WriteLine(t[0] + " " + t[1] + " " + t[2] + " " + t[3] + " " + t[4] + " " + t[5] + " " + t[6]);
+
+            // write to file
+            Employee.WriteToFile(empl2);
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
